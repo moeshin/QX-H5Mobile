63885fda4719ceb4c13e94994a9817d1d05436c5
MoeShin <hi@moeshin.com>
2023-10-13 20:05:29 +0800

    20231010

--- 20230922/src/views/Home.vue
+++ 20230922/src/views/Home.vue
@@ -3,7 +3,41 @@
     <h2 v-html="subTitle"></h2>
     <p><img :src="src"></p>
     <p><button @click="onClick">点赞：{{ count }}</button></p>
+    <div class="flex-container">
+        <div>Box1</div>
+        <div>Box2</div>
+        <div>Box3</div>
+    </div>
 </template>
+<style scoped lang="less">
+.flex-container {
+    display: flex;
+    height: 50vh;
+
+    > * {
+        display: flex;
+        height: 100%;
+        border: 1px solid;
+        justify-content: center;
+        align-items: center;
+    }
+
+    >:nth-child(1) {
+        flex-basis: 200px;
+        border-color: red;
+    }
+
+    >:nth-child(2) {
+        flex-grow: 1;
+        border-color: yellow;
+    }
+
+    >:nth-child(3) {
+        flex-basis: 200px;
+        border-color: green;
+    }
+}
+</style>
 <script lang="ts">
 export default {
     data: () => ({
