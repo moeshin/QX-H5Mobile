e911ca2022218f8ba4cd577176ea087d0a182a7b
MoeShin <hi@moeshin.com>
2023-11-09 00:10:00 +0800

    custom email verification rule

--- my_lesson_h5/src/plugins/yup.ts
+++ my_lesson_h5/src/plugins/yup.ts
@@ -3,4 +3,19 @@ import * as yup from 'yup';
 
 export function init() {
   yup.setLocale(yupLocales.zh);
+  // yup.addMethod(
+  //   yup.string,
+  //   'email',
+  //   function (message = yup.defaultLocale.string?.email) {
+  //     // https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+  //     return this.matches(
+  //       /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/,
+  //       {
+  //         name: 'email',
+  //         message,
+  //         excludeEmptyString: true,
+  //       },
+  //     );
+  //   },
+  // );
 }
