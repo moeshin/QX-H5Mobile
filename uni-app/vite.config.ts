e76ca380da5030205cc1ec2d000dea7cc5aae467
MoeShin <hi@moeshin.com>
2023-12-26 17:56:52 +0800

    chore: pre build

--- uni-app/vite.config.ts
+++ uni-app/vite.config.ts
@@ -1,15 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import fs from 'node:fs';
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import uni from '@dcloudio/vite-plugin-uni';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
+    {
+      name: 'local.config.ts',
+      buildStart() {
+        const path = 'src/local.config.ts';
+        if (!fs.existsSync(path)) {
+          fs.writeFileSync(path, '');
+        }
+      },
+    },
     uni(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
