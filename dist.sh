093898b719ddb773b8bbf2a0804d775edb9a9444
MoeShin <hi@moeshin.com>
2023-12-26 17:38:06 +0800

    chore: github workflow

--- dist.sh
+++ dist.sh
@@ -12,14 +12,19 @@ echo '<!DOCTYPE html>
 <body>' >>dist/index.html
 for project in */; do
   project="${project::-1}"
+  if [ "$project" == dist ]; then
+    continue
+  fi
   out_dir="$project/dist"
   if ! [ -f "$out_dir/index.html" ]; then
-    if [ -d "$out_dir/build/h5/index.html" ]; then
+    # uni-app
+    if [ -f "$out_dir/build/h5/index.html" ]; then
       out_dir="$out_dir/build/h5"
     fi
   fi
   echo "out_dir: $out_dir"
   if [ -f "$out_dir/index.html" ]; then
+    echo mv "$out_dir" "dist/$project"
     mv "$out_dir" "dist/$project"
     echo "<li><a href=\"./$project\">$project</a></li>" >>dist/index.html
   fi
